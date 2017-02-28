a51b48f9bdd09d5166d061a4eafc6627be3d94b7
Erika Bourque <erika.bourque@dawsoncollege.qc.ca>
2017-02-28 16:19:19 -0500

    Added calendars to report page, need to link to report query

--- src/main/java/com/pandamedia/beans/reportBackingBean.java
+++ src/main/java/com/pandamedia/beans/reportBackingBean.java
@@ -3,6 +3,8 @@ package com.pandamedia.beans;
 import java.io.Serializable;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * This class provides common methods for the report pages.
@@ -13,8 +15,39 @@ import javax.inject.Named;
 @RequestScoped
 public class reportBackingBean implements Serializable{
     
-    public String getCurrentDate()
+    private Date startDate;
+    private Date endDate;
+    
+    public Date getDefaultEndDate()
+    {               
+        return Calendar.getInstance().getTime();
+    }
+    
+    public Date getDefaultStartDate()
     {
-        return null;
+        Calendar start = Calendar.getInstance();
+        start.add(Calendar.DAY_OF_YEAR, -30);
+        
+        return start.getTime();
+    }
+    
+    public Date getStartDate()
+    {
+        return startDate;
+    }
+    
+    public Date getEndDate()
+    {
+        return endDate;
+    }
+    
+    public void setStartDate(Date date)
+    {
+        startDate = date;
+    }
+    
+    public void setEndDate(Date date)
+    {
+        endDate = date;
     }
 }
