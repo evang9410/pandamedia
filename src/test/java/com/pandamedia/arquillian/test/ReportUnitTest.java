2f8cb91381f23c86fc2ee8cc6812fe07315e15c1
Erika Bourque <erika.bourque94@gmail.com>
2017-03-07 21:57:37 -0500

    Starting to write test for zero user

--- src/test/java/com/pandamedia/arquillian/test/ReportUnitTest.java
+++ src/test/java/com/pandamedia/arquillian/test/ReportUnitTest.java
@@ -1,7 +1,21 @@
 package com.pandamedia.arquillian.test;
 
 import com.pandamedia.beans.ReportBackingBean;
+import com.pandamedia.beans.ReportDateBean;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.sql.DataSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -9,8 +23,12 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import persistence.controllers.ShopUserJpaController;
 import persistence.controllers.exceptions.RollbackFailureException;
+import persistence.entities.ShopUser;
 import persistence.entities.Track;
 
 /**
@@ -19,6 +37,18 @@ import persistence.entities.Track;
  */
 @RunWith(Arquillian.class)
 public class ReportUnitTest {
+    @Resource(name = "java:app/g4w17test")
+    private DataSource ds;
+    
+    @Inject
+    private ReportBackingBean reports;
+
+    @Inject
+    private ReportDateBean dates;
+    
+    @Inject
+    private ShopUserJpaController userJpa;
+    
     @Deployment
     public static WebArchive deploy() {
 
@@ -48,5 +78,79 @@ public class ReportUnitTest {
                 .addAsLibraries(dependencies);
 
         return webArchive;
+    }
+    
+    /**
+     * This routine is courtesy of Bartosz Majsak who also solved my Arquillian
+     * remote server problem
+     */
+    @Before
+    public void seedDatabase() {
+        final String seedDataScript = loadAsString("createtables.sql");
+
+        try (Connection connection = ds.getConnection()) {
+            for (String statement : splitStatements(new StringReader(
+                    seedDataScript), ";")) {
+                connection.prepareStatement(statement).execute();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed seeding database", e);
+        }
+        //System.out.println("Seeding works");
+    }
+
+    /**
+     * The following methods support the seedDatabse method
+     */
+    private String loadAsString(final String path) {
+        try (InputStream inputStream = Thread.currentThread()
+                .getContextClassLoader().getResourceAsStream(path)) {
+            return new Scanner(inputStream).useDelimiter("\\A").next();
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to close input stream.", e);
+        }
+    }
+
+    private List<String> splitStatements(Reader reader,
+            String statementDelimiter) {
+        final BufferedReader bufferedReader = new BufferedReader(reader);
+        final StringBuilder sqlStatement = new StringBuilder();
+        final List<String> statements = new LinkedList<>();
+        try {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty() || isComment(line)) {
+                    continue;
+                }
+                sqlStatement.append(line);
+                if (line.endsWith(statementDelimiter)) {
+                    statements.add(sqlStatement.toString());
+                    sqlStatement.setLength(0);
+                }
+            }
+            return statements;
+        } catch (IOException e) {
+            throw new RuntimeException("Failed parsing sql", e);
+        }
+    }
+
+    private boolean isComment(final String line) {
+        return line.startsWith("--") || line.startsWith("//")
+                || line.startsWith("/*");
+    }
+    
+    /**
+     *
+     * @throws SQLException
+     */
+    @Test
+    public void findZeroShopUser() throws SQLException {
+        // Set Up
+        ShopUser bob = new ShopUser();
+        
+//        List<ShopUser> users = fab.getAll();
+//        assertThat(lfd).hasSize(200);
     }
 }
