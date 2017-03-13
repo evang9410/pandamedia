9bcdfe26ada0d1e47425ce299b4848e4849294c1
Hau Gilles Che <haugillesche@gmail.com>
2017-03-12 20:54:39 -0400

    All input fields present, no exception but user not created

--- src/main/java/com/pandamedia/beans/UserBean.java
+++ src/main/java/com/pandamedia/beans/UserBean.java
@@ -6,10 +6,12 @@ import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
@@ -22,13 +24,14 @@ import persistence.entities.ShopUser;
 import jodd.mail.*;
 import persistence.controllers.ProvinceJpaController;
 import persistence.controllers.ShopUserJpaController;
+import persistence.entities.Province;
 
 /**
  *
  * @author Hau Gilles Che
  */
 @Named
-@SessionScoped
+@RequestScoped
 public class UserBean implements Serializable {
 
     @Inject
@@ -40,8 +43,14 @@ public class UserBean implements Serializable {
     private String password;
     private String confirmPasswd;
     //used to salt the password
-    private static SecureRandom random = new SecureRandom();
+    private  SecureRandom random;
+    private List<Province> provinces;
 
+    @PostConstruct
+    public void init(){
+        provinces=provinceController.findProvinceEntities();
+        random = new SecureRandom();
+    }
     public ShopUser getShopUser() {
         return shopUser;
     }
@@ -65,6 +74,16 @@ public class UserBean implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public List<Province> getProvinces() {
+        return provinces;
+    }
+
+    public void setProvinces(List<Province> provinces) {
+        this.provinces = provinces;
+    }
+    
+    
 
     public String createNewUser() throws Exception {
         setFields();
@@ -97,19 +116,20 @@ public class UserBean implements Serializable {
 
     public void setFields() {
         //set address informations empty, to be filled later
+        /*
         shopUser.setCity("");
         shopUser.setCountry("");
         shopUser.setHomePhone("");
         shopUser.setPostalCode("");
         shopUser.setStreetAddress("");
         shopUser.setProvinceId(provinceController.findProvince(1));
-        
+        */
         //generates a random salt to salt the password
         String salt=getSalt();
         byte[] hashedPwd=hash(password,salt);
         
         shopUser.setSalt(salt);
-        shopUser.setPassword(hashedPwd);
+        shopUser.setHashedPw(hashedPwd);
         
     }
 
