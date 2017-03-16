5d402bcd47b89a2aefe1d30f89178360689bb04b
Hau Gilles Che <haugillesche@gmail.com>
2017-03-15 21:11:04 -0400

    Postal code & email validation working, missing phone number validation and password validation

--- src/main/java/com/pandamedia/beans/RegistrationActionBean.java
+++ src/main/java/com/pandamedia/beans/RegistrationActionBean.java
@@ -15,8 +15,13 @@ import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import javax.inject.Inject;
 import javax.inject.Named;
+import jodd.mail.EmailAddress;
 import persistence.controllers.ProvinceJpaController;
 import persistence.controllers.ShopUserJpaController;
 import persistence.entities.Province;
@@ -37,12 +42,11 @@ public class RegistrationActionBean implements Serializable {
 
     @Inject
     private UserBean userBean;
-    
+
     @Inject
     private ProvinceJpaController provinceController;
 
     // Needed variables.
-    //private SecureRandom secureRand;
     private ShopUser loggedUser;
     private List<Province> provinces;
     private Province province;
@@ -50,38 +54,32 @@ public class RegistrationActionBean implements Serializable {
     @PostConstruct
     public void init() {
         provinces = provinceController.findProvinceEntities();
-       // secureRand = new SecureRandom();
     }
-
+    
+    /**
+     * Responsible for creating a new user
+     * @param province Id of the province chosen by the user.
+     */
     public void register(String province) {
-        System.out.println(province);
         ShopUser user = userBean.getShopUser();
-        if (!userController.checkIfUserExists(user)) {
-            // User does not exist.
-            setFields(user);
-            int provinceId=Integer.parseInt(province);
-            Province prov=provinceController.findProvince(provinceId);
-            System.out.println(prov.getName()+" "+prov.getId());
-            user.setProvinceId(prov);
-            System.out.println(provinceId);
-            try {
-                if(province == null)
-                    Logger.getLogger(RegistrationActionBean.class.getName()).log(Level.SEVERE, null, "User created");
-                //user.setProvinceId(provinces.get(0));
-                userController.create(user);
-                loggedUser = user;
-                Logger.getLogger(RegistrationActionBean.class.getName()).log(Level.SEVERE, null, "User created");
-            } catch (Exception ex) {
-                Logger.getLogger(RegistrationActionBean.class.getName()).log(Level.SEVERE, null, "RollBackException");
-            }
-
-        } else {
-            // Display error
-            Logger.getLogger(RegistrationActionBean.class.getName()).log(Level.SEVERE, null, "Non Unique user");
+        setFields(user);
+        int provinceId = Integer.parseInt(province);
+        Province prov = provinceController.findProvince(provinceId);
+        user.setProvinceId(prov);
+        try {
+            userController.create(user);
+            loggedUser = user;
+            Logger.getLogger(RegistrationActionBean.class.getName()).log(
+                    Level.SEVERE, null, "User created");
+        } catch (Exception ex) {
+            FacesMessage msg = com.pandamedia.utilities.Messages.getMessage(
+                    "bundles.messages", "duplicateEmail", null);
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            FacesContext.getCurrentInstance().addMessage(
+                    "registrationForm:emailInput", msg);
         }
     }
 
-    /* Getters and Setter*/
     public ShopUser getLoggedUser() {
         return loggedUser;
     }
@@ -98,18 +96,40 @@ public class RegistrationActionBean implements Serializable {
         this.province = province;
     }
     
-
+    /**
+     * Sets the remaining fields that cannot be directly set by the user 
+     * through the form.
+     * @param user 
+     */
     private void setFields(ShopUser user) {
         //instantiating the class responsible for password security
-        PasswordHelper pwdHelper = new PasswordHelper(); 
+        PasswordHelper pwdHelper = new PasswordHelper();
         String salt = pwdHelper.getSalt();
 
         byte[] hashedPwd = pwdHelper.hash("FuckPriver", salt);
 
         user.setSalt(salt);
         user.setHashedPw(hashedPwd);
-        
+
         user.setPostalCode(userBean.getPostalCode().toString());
+    }
+
+    /**
+     * Validates the email address to make sure it is properly formatted
+     *
+     * @param fc
+     * @param c
+     * @param value
+     */
+    public void validateEmail(FacesContext fc, UIComponent c, Object value) {
+        EmailAddress email = new EmailAddress((String) value);
+
+        if (!email.isValid()) {
+            FacesMessage msg = com.pandamedia.utilities.Messages.getMessage(
+                    "bundles.messages", "badEmailFormat", null);
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(msg);
+        }
     }
 
 }
--- src/main/java/com/pandamedia/converters/PostalCodeConverter.java
+++ src/main/java/com/pandamedia/converters/PostalCodeConverter.java
@@ -2,6 +2,7 @@ package com.pandamedia.converters;
 
 import com.pandamedia.beans.PostalCodeBean;
 import java.io.Serializable;
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
@@ -12,14 +13,15 @@ import javax.faces.convert.FacesConverter;
  *
  * @author Hau Gilles Che
  */
-@FacesConverter(forClass=PostalCodeBean.class)
+@FacesConverter(forClass = PostalCodeBean.class)
 public class PostalCodeConverter implements Converter, Serializable {
+
     private String separator;
-    
-    public void setSeparator(String separator){
-        this.separator=separator;
+
+    public void setSeparator(String separator) {
+        this.separator = separator;
     }
-    
+
     /**
      * Used when inputing a value Accept the string and check that includes
      * character or whitespace
@@ -32,33 +34,52 @@ public class PostalCodeConverter implements Converter, Serializable {
     @Override
     public Object getAsObject(FacesContext context, UIComponent component,
             String newValue) throws ConverterException {
-        StringBuilder builder=new StringBuilder(newValue);
-        boolean foundInvalidCharacter=false;
-        char invalidChar;
-        int i=0;
-        while(i<builder.length() && !foundInvalidCharacter){
-            char ch=builder.charAt(i);
-            if(Character.isAlphabetic(ch)){
-                char tempChar=Character.toUpperCase(ch);
-                builder.setCharAt(i, tempChar);
-                i++;
-            }else if(Character.isDigit(ch)){
-                i++;
-            }else if(Character.isWhitespace(ch)){
-                    builder.deleteCharAt(i);              
-            }else{
-                foundInvalidCharacter=true;
-                invalidChar=ch;
+        StringBuilder builder = new StringBuilder(newValue);
+        boolean foundInvalidCharacter = false;
+        char invalidChar = '\0';
+        int i = 0;
+        while (i < builder.length() && !foundInvalidCharacter) {
+            char ch = builder.charAt(i);
+            if (i % 2 == 0) {
+                if (Character.isAlphabetic(ch)) {
+                    char tempChar = Character.toUpperCase(ch);
+                    builder.setCharAt(i, tempChar);
+                    i++;
+                } else {
+                    foundInvalidCharacter = true;
+                    invalidChar = ch;
+                }
+            } else {
+                if (Character.isDigit(ch)) {
+                    i++;
+                } else if (Character.isWhitespace(ch)) {
+                    builder.deleteCharAt(i);
+                } else {
+                    foundInvalidCharacter = true;
+                    invalidChar = ch;
+                }
             }
         }
-        /*
-        if(foundInvalidCharacter){
-            FacesMessage msg=
+
+        if (foundInvalidCharacter) {
+            FacesMessage msg = com.pandamedia.utilities.Messages.getMessage(
+                    "bundles.messages", "badPostalCodeCharacter",
+                    new Object[]{invalidChar});
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ConverterException(msg);
         }
-*/
+        
+        //checks for invalid amount of characters
+        if (i != 6) {
+            FacesMessage msg = com.pandamedia.utilities.Messages.getMessage(
+                    "bundles.messages", "badPostalCodeLength", new Object[]{i});
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ConverterException(msg);
+        }
+
         return new PostalCodeBean(builder.toString());
     }
-    
+
     /**
      * Used when display the value Depending on the length of the string add
      * space
@@ -71,24 +92,25 @@ public class PostalCodeConverter implements Converter, Serializable {
     @Override
     public String getAsString(FacesContext context, UIComponent component,
             Object value) throws ConverterException {
-        if(!(value instanceof PostalCodeBean)){
+        if (!(value instanceof PostalCodeBean)) {
             throw new ConverterException();
         }
-        String v=((PostalCodeBean)value).toString();
-        String sep=separator;
-        if(sep==null){
-            sep=" ";
+        String v = ((PostalCodeBean) value).toString();
+        String sep = separator;
+        if (sep == null) {
+            sep = " ";
         }
         //if length is 7, space is already there
-        if(v.length() == 7)
+        if (v.length() == 7) {
             return v;
-        
+        }
+
         //inserts the space in the middle
-        StringBuilder result=new StringBuilder();
-        result.append(v.substring(0,3));
+        StringBuilder result = new StringBuilder();
+        result.append(v.substring(0, 3));
         result.append(sep);
         result.append(v.substring(3));
-        
+
         return result.toString();
     }
 }
