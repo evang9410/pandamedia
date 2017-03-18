304b7429f1118462dc77072fbd73b7fa32ab762b
Erika Bourque <erika.bourque94@gmail.com>
2017-03-18 15:47:38 -0400

    Rearranged cart to use 2 lists, add / remove methods for lists

--- src/main/java/com/pandamedia/beans/ShoppingCart.java
+++ src/main/java/com/pandamedia/beans/ShoppingCart.java
@@ -26,119 +26,152 @@ import persistence.entities.Album;
  */
 @Named("cart")
 @SessionScoped
-public class ShoppingCart implements Serializable{
-    private List<Object> cart;
+public class ShoppingCart implements Serializable {
+//    private List<Object> cart;
+
+    private List<Album> albums;
+    private List<Track> tracks;
     private UIViewRoot prevPage;
     @Inject
     private UserBackingBean user;
-    public ShoppingCart(){
-        this.cart = new ArrayList();
+
+    public ShoppingCart() {
+//        this.cart = new ArrayList();
+        albums = new ArrayList<>();
+        tracks = new ArrayList<>();
     }
+
     /**
      * returns the list of items in the shopping cart.
-     * @return 
+     *
+     * @return
      */
-    public List<Object> getCart(){
-        return this.cart;
-    }
+//    public List<Object> getCart(){
+//        return this.cart;
+//    }
     /**
-     * Gathers the shopping cart album objects and returns them as a list to be displayed in the cart.
-     * @return 
+     * Gathers the shopping cart album objects and returns them as a list to be
+     * displayed in the cart.
+     *
+     * @return
      */
-    public List<Album> getAlbumsFromCart(){
-        List<Album> albums = new ArrayList();
-        for(int i = 0; i < cart.size(); i++){
-            if(cart.get(i) instanceof Album){
-                albums.add((Album)cart.get(i));
-            }
-        }
+    public List<Album> getAlbumsFromCart() {
+//        List<Album> albums = new ArrayList();
+//        for(int i = 0; i < cart.size(); i++){
+//            if(cart.get(i) instanceof Album){
+//                albums.add((Album)cart.get(i));
+//            }
+//        }
         return albums;
     }
+
     /**
-     * gets the tracks from the cart.
-     * There has to be a more efficient way to sort this list. Maybe a hashmap?
-     * @return 
+     * gets the tracks from the cart. There has to be a more efficient way to
+     * sort this list. Maybe a hashmap?
+     *
+     * @return
      */
-    public List<Track> getTracksFromCart(){
-        List<Track> tracks = new ArrayList();
-        for(int i = 0; i < cart.size(); i++){
-            if(cart.get(i) instanceof Track){
-                tracks.add((Track)cart.get(i));
-            }
-        }
+    public List<Track> getTracksFromCart() {
+//        List<Track> tracks = new ArrayList();
+//        for(int i = 0; i < cart.size(); i++){
+//            if(cart.get(i) instanceof Track){
+//                tracks.add((Track)cart.get(i));
+//            }
+//        }
         return tracks;
     }
-    
-    public double getSubTotal(){
+
+    public double getSubTotal() {
         double subtotal = 0;
-        for(Object o : cart){
-            if(o instanceof Album){
-                subtotal += ((Album) o).getListPrice() - ((Album) o).getSalePrice();
-            }
-            if(o instanceof Track){
-                subtotal += ((Track)o).getListPrice() - ((Track)o).getSalePrice();
-            }
+        for (Album a : albums) {
+            subtotal += a.getListPrice() - a.getSalePrice();
         }
+        for (Track t : tracks) {
+            subtotal += t.getListPrice() - t.getSalePrice();
+        }
+//        for(Object o : cart){
+//            if(o instanceof Album){
+//                subtotal += ((Album) o).getListPrice() - ((Album) o).getSalePrice();
+//            }
+//            if(o instanceof Track){
+//                subtotal += ((Track)o).getListPrice() - ((Track)o).getSalePrice();
+//            }
+//        }
         return subtotal;
     }
+
     /**
      * returns the amount of items in the cart, to be used by the navigation bar
      * to display the amount of items currently in the cart.
-     * @return 
-     */
-    public String getCartCount(){
-        if(cart.size() > 0)
-            return " ( " + this.cart.size() +" )";
-        else
-            return "";
-    }
-    
-    public boolean getIsCartEmpty(){
-        boolean isEmpty = false;
-        if(cart.size() == 0){
-           isEmpty= true;
-        }
-        return isEmpty;
-    }
-    public void removeAlbumFromCart(Album a){
- 
-        
-    }
-    
-    public void removeItem(Object o){
-        cart.remove(o);
-    }
-    
-    
-    /**
-     * Sets the UIViewRoot object, to be called when the shopping cart icon
-     * in the navigation bar is clicked is clicked.
+     *
      * @return
      */
-    public String setPrevPage(){
+    public String getCartCount() {
+        int size = albums.size() + tracks.size();
+//        if(cart.size() > 0)
+//            return " ( " + this.cart.size() +" )";
+//        else
+//            return "";
+        if (size > 0) {
+            return " ( " + size + " )";
+        } else {
+            return "";
+        }
+    }
+
+    public boolean getIsCartEmpty() {
+//        boolean isEmpty = false;
+//        if(cart.size() == 0){
+//           isEmpty= true;
+//        }
+//        return isEmpty;
+        return (albums.size() + tracks.size()) == 0;
+    }
+
+    public void removeAlbumFromCart(Album a) {
+        albums.remove(a);
+    }
+
+    public void removeTrackFromCart(Track t) {
+        tracks.remove(t);
+    }
+
+//    public void removeItem(Object o){
+//        cart.remove(o);
+//    }
+    /**
+     * Sets the UIViewRoot object, to be called when the shopping cart icon in
+     * the navigation bar is clicked is clicked.
+     *
+     * @return
+     */
+    public String setPrevPage() {
         prevPage = FacesContext.getCurrentInstance().getViewRoot();
         return "cart";
     }
+
     /**
-     * returns the user to the location of the ui where the prevPage 
-     * object is holding.
-     * If the prevPage is null or not defined, return them to the mainpage.
+     * returns the user to the location of the ui where the prevPage object is
+     * holding. If the prevPage is null or not defined, return them to the
+     * mainpage.
      */
-    public void continueShopping() throws IOException{
-        if(prevPage != null){
+    public void continueShopping() throws IOException {
+        if (prevPage != null) {
             FacesContext.getCurrentInstance().setViewRoot(prevPage);
             FacesContext.getCurrentInstance().renderResponse();
-        }else{
-             ExternalContext context = FacesContext.getCurrentInstance().getExternalContext(); 
-             context.redirect(context.getRequestContextPath() + "/mainpage.xhtml");
-             System.out.println(context.getRequestContextPath());
+        } else {
+            ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+            context.redirect(context.getRequestContextPath() + "/mainpage.xhtml");
+            System.out.println(context.getRequestContextPath());
         }
-        
+
     }
-    
-    public void addAlbum(Album album)
-    {
-        cart.add(album);
+
+    public void addAlbum(Album album) {
+        albums.add(album);
     }
-    
+
+    public void addTrack(Track track) {
+        tracks.add(track);
+    }
 }
