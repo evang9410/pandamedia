47ece1b723cbcff17774db8b3735eef532e29b3c
Jason Citron <pazelart@gmail.com>
2017-03-23 16:25:40 -0400

    Added both browse artists page and individual artist page

--- src/main/java/com/pandamedia/beans/ArtistBackingBean.java
+++ src/main/java/com/pandamedia/beans/ArtistBackingBean.java
@@ -5,20 +5,40 @@
  */
 package com.pandamedia.beans;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import persistence.controllers.ArtistJpaController;
 import persistence.entities.Artist;
+
 
 /**
  *
- * @author Erika Bourque
+ * @author Pierre Azelart
  */
-@Named
-@RequestScoped
-public class ArtistBackingBean {
+@Named("artistBacking")
+@SessionScoped
+public class ArtistBackingBean implements Serializable{
     private static final Logger LOG = Logger.getLogger("ArtistBackingBean.class");
+    @Inject
+    private ArtistJpaController artistController;
     private Artist artist;
+    private List artists;
+    @PersistenceContext
+    private EntityManager em;
+    
+    public String artistPage(Artist a){
+        this.artist = a;
+        LOG.info("" + a.getId() +"\n" + a.getName() +"\n");
+        return "artist";
+    }
     
     public Artist getArtist()
     {
@@ -29,5 +49,16 @@ public class ArtistBackingBean {
     {
         LOG.info("New artist id: " + artist.getId());
         this.artist = artist;
+    }
+    
+    public List<Artist> getArtistsList(){
+        String q = "SELECT a FROM Artist a";
+        TypedQuery<Artist> query = em.createQuery(q, Artist.class);
+        this.artists = query.getResultList();
+        return this.artists;
+    }
+    
+    public void setArtistList(ArrayList<Artist> a){
+        this.artists = a;
     }
 }
