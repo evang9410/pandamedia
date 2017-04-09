d68e167535007f1f1debafeb921d9f0606eaea5c
Hau Gilles Che <haugillesche@gmail.com>
2017-04-09 14:21:23 -0400

    trying to test

---
+++ src/test/java/com/pandamedia/selenium/tests/SurveyTestIT.java
@@ -0,0 +1,41 @@
+package com.pandamedia.selenium.tests;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ *
+ * @author Hau Gilles Che
+ */
+public class SurveyTestIT {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    
+    @Before
+    public  void setUp(){
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+    }
+    
+    @Test
+    public void testSurvey() throws Exception{
+        driver.get("http://localhost:8080/pandamedia/mainpage.xhtml");
+        wait=new WebDriverWait(driver,10);
+        
+        driver.findElement(By.xpath("//div[contains(@id,'surveyFormDiv')]/div[1]/div/div/div/div/div/div/input")).click();
+        driver.findElement(By.id("j_idt96:submitBtn")).click();
+        wait = new WebDriverWait(driver,10);
+        wait.until(new ExpectedCondition<Boolean>(){
+            @Override
+            public Boolean apply(WebDriver d){
+                return false;
+            }
+        });
+    }
+}
